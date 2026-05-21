eb497baf0c5bfc4a28b35a6afcd7c88135072a82
deanpetersonstu-jpg <dean.peterson.stu@cedarpark.org>
2026-05-21 09:06:49 -0700

    Final Commit

--- platformGame.py
+++ platformGame.py
@@ -25,24 +25,25 @@ Rect((0, 0), (535, 100))
 
 # Collectibles
 coins = [
-Rect((250, 340), (50, 30)),
-Rect((400, 260), (50, 30)),
-Rect((200, 775), (50, 30)),
-Rect((150, 650), (50, 30))
+Rect((401.625, 650 ), (50, 30)),
+Rect((270, 550 ), (50, 30)),
+Rect((133.875, 450 ), (50, 30)),
+Rect((20, 300 ), (50, 30)),
+Rect((133.875, 150 ), (50, 30)),
 ]
 score = 0
+lives = 2
 
 # Hazards and goal
 lavas = [ 
 Rect((450, 701), (20, 200)),
-Rect((250, 701), (200, 20)),
 Rect((250, 501), (20, 200)),
 Rect((310, 635), (300, 20)),
 Rect((310, 435), (20, 200)),
 Rect((60, 435), (250, 20)),
 Rect((60, 200), (20, 250)),
-Rect((60, 201), (300, 20)),
 Rect((360, 0), (20, 220)),
+Rect((0, 130), (315, 20)),
 ]
 goal = Rect((0, 0), (535, 100))
 game_won = False
@@ -62,6 +63,7 @@ def draw():
         screen.draw.filled_rect(coin, "chocolate4")
 
     screen.draw.text(f"Score: {score}", (10, 100), fontsize=30, color="white")
+    screen.draw.text(f"Lives: {lives}", (10, 150), fontsize=30, color="white")
 
     for lava in lavas:
         screen.draw.filled_rect(lava, "red")
@@ -73,8 +75,13 @@ def draw():
         screen.draw.text("You Win!", center=(267.5, 500), fontsize=60, color="yellow")
         screen.draw.text(f"Score: {score}", center = (267.5, 600), fontsize = 30, color = "white")
 
+    if lives == 0:
+        screen.clear()
+        screen.draw.text(f"GAME OVER!", (175, 500), fontsize=50, color="red")
+        screen.draw.text(f"Score: {score}", (10, 100), fontsize=30, color="white")
+        screen.draw.text(f"Lives: {lives}", (10, 150), fontsize=30, color="white")
 def update():
-    global velocity_y, on_ground
+    global velocity_y, on_ground, lives
     if keyboard.left:
         player.x -= 5
 
@@ -117,6 +124,7 @@ def update():
             player.x = 500
             player.y = 900
             velocity_y = 0
+            lives -= 1
 
     global game_won
 
